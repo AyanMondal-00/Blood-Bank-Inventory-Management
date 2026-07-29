b8319bad4fcc5684302889ddbbdb77fb7c96d056
Ayan Mondal <ayanmonal0914@gmail.com>
2026-07-29 11:29:31 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1>Blood Bank Inventory Management System</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
