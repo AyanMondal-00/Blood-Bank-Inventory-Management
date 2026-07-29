7d4efab51dbdaaeb12fa12b18e87193a0a00f1b0
Ayan Mondal <ayanmonal0914@gmail.com>
2026-07-29 18:34:56 +0530

    add all

--- server/src/middlewares/inventoryValidation.js
+++ server/src/middlewares/inventoryValidation.js
@@ -7,9 +7,45 @@ export const validateCreateInventory = (req, res, next) => {
     blood_type,
     government_price,
     received_unit,
+    available_unit,
     expiry_date,
   } = req.body;
+  const validBloodGroups = [
+  "A+",
+  "A-",
+  "B+",
+  "B-",
+  "AB+",
+  "AB-",
+  "O+",
+  "O-",
+];
+if (!Number.isInteger(received_unit) || received_unit <= 0) {
+  throw new ApiError(400, "Received unit must be a positive integer");
+}
+if (
+  typeof government_price !== "number" ||
+  government_price < 0
+) {
+  throw new ApiError(
+    400,
+    "Government price must be a valid positive number"
+  );
+}
 
+if (!validBloodGroups.includes(blood_type)) {
+  throw new ApiError(400, "Invalid blood type");
+}
+
+const entryDate = new Date(entry_date);
+const expiryDate = new Date(expiry_date);
+
+if (expiryDate <= entryDate) {
+  throw new ApiError(
+    400,
+    "Expiry date must be greater than entry date"
+  );
+}
   if (!entry_date) {
     throw new ApiError(400, "Entry date is required");
   }
@@ -29,6 +65,10 @@ export const validateCreateInventory = (req, res, next) => {
   if (received_unit === undefined) {
     throw new ApiError(400, "Received unit is required");
   }
+    if (available_unit === undefined) {
+    throw new ApiError(400, "Available unit is required");
+  }
+
 
   if (!expiry_date) {
     throw new ApiError(400, "Expiry date is required");
--- server/src/routes/inventoryRoutes.js
+++ server/src/routes/inventoryRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAllInventory } from "../controllers/inventoryController.js";
 import { createInventory } from "../controllers/inventoryController.js";
+import { validateCreateInventory } from "../middlewares/inventoryValidation.js";
 
 const router = Router();
 
 router.get("/", getAllInventory);
-router.post("/", createInventory);
+router.post("/", validateCreateInventory, createInventory);
 
 export default router;
